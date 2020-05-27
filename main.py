03f6aeb0fddd399dfbf6aa1af6296d99f1a77b74
Owolabi Oromidayo <owolabi.oromidayo@stu.cu.edu.ng>
2020-05-27 09:47:08 +0100

    testing stash pop

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os, threading
-import server, chatroom, client
+import os
+import threading
+import server
+import chatroom
+import client
+
 
 def server_thread():
     server.run()
@@ -9,24 +13,13 @@ def app_thread():
     chatroom.ChatRoom()
 
 
-# def reader_thread():
-#     client.run('Anonymous', reader=True)
-
-
-# def client_thread():
-#     name = input('Name: ')
-#     client.run(name)
-
-
-
-
 if __name__ == "__main__":
 
     threads = [threading.Thread(target=server_thread),
-                # threading.Thread(target=client_thread),
-                threading.Thread(target=app_thread)
-                # threading.Thread(target=reader_thread)
-                ]
+               # threading.Thread(target=client_thread),
+               threading.Thread(target=app_thread)
+               # threading.Thread(target=reader_thread)
+               ]
 
     for thread in threads:
         thread.start()
---
+++ server_class.py
@@ -0,0 +1,85 @@
+import socket, select
+import file_handler
+
+
+HEADER_LENGTH = 10
+IP = "127.0.0.1"
+PORT = 1234
+
+class Server:
+    def __init__(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #To reuse same port on restart
+        self.socket.bind((IP, PORT))
+
+        self.socket.listen()
+
+        self.sockets_list = [self.socket]
+
+        self.clients = {}
+
+        self.run()
+
+
+    def receive_message(self, client_socket):
+        try:
+            message_header = client_socket.recv(HEADER_LENGTH)
+
+            if not len(message_header):
+                return False
+
+            message_length = int(message_header.decode('utf-8').strip()) 
+            return {"header": message_header, "data": client_socket.recv(message_length)}
+
+        except:
+            return False
+
+    
+    def run(self):
+            while True:
+                read_sockets , _, exception_sockets = select.select(self.sockets_list, [], self.sockets_list)
+                for notified_socket in read_sockets:
+                    if notified_socket == self.socket:
+                        client_socket, client_address = self.socket.accept()
+
+                        user = self.receive_message(client_socket)
+                        if user is False:
+                            continue
+                            
+                        self.sockets_list.append(client_socket)
+                        self.clients[client_socket] = user
+
+                        file_handler.send(F"Accepted new connection from {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf-8')}")
+
+                    else:
+                        message = self.receive_message(notified_socket)
+
+                        if message is False:
+                            print(f"Closed connection from {self.clients[notified_socket]['data'].decode('utf-8')}")
+                            self.sockets_list.remove(notified_socket)
+                            del self.clients[notified_socket]
+                            continue
+
+                        user = self.clients[notified_socket]
+                        file_handler.send(f"Received message from {user['data'].decode('utf-8')} : {message['data'].decode('utf-8')}  ")
+
+                        for client_socket in self.clients:
+                            if client_socket != notified_socket:
+                                client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+
+                for notified_socket in exception_sockets:
+                    self.sockets_list.remove(notified_socket)
+                    del self.clients[notified_socket]
+
+
+if __name__ == "__main__":
+    Server()
+
+
+
+    
+
+
+    
+
+        
