839fd6e6bf389328b8cb9d71f7d8083f590aa505
Owolabi Oromidayo <owolabi.oromidayo@stu.cu.edu.ng>
2020-05-27 11:37:26 +0100

    Scrollbar implemented

--- modules/chatroom.py
+++ modules/chatroom.py
@@ -1,5 +1,5 @@
-import tkinter as tk
 import time
+import tkinter as tk
 import modules.client as client
 import modules.prompt_window as prompt_window
 import utils.file_handler as file_handler
@@ -12,73 +12,79 @@ class ChatRoom:
         singlethread(self.handle_chatroom)
 
     def handle_chatroom(self):
+        self.x, self.y = 200, 20
         self.username = ''
-        prompt_window.PromptWindow(self, 'Input Username to Proceed:')
-        # self.get_name()
         self.prev_msg = ''
-        self.x = 200
-        self.y = 0
+
+        prompt_window.PromptWindow(self, 'Input Username to Proceed:')
         self.client = client.Client(self.username)
         multithread([self.build, self.listen], 0.02)
 
     def build(self):
         self.root = tk.Tk()
-        self.root.title("Sock Servc")
+        self.root.title("Chatroom")
         self.root.bind('<Return>', self.handle_click)
+        self.root.resizable(width=False, height=False)
 
-        self.canvas = tk.Canvas(self.root, width=800, height=600)
-        self.canvas.pack()
+        # CANVAS AND SCROLLBAR
+        self.canvas_frame = tk.Frame(self.root, width=800, height=600)
+        self.canvas_frame.pack(expand=True, fill=tk.BOTH)
 
-        self.label = tk.Label(self.root, text=self.username)
+        self.canvas = tk.Canvas(
+            self.canvas_frame, width=800, height=600, bg="#FFFFFF", scrollregion=(0, 0, 800, 2000)
+        )
+
+        self.canvas_scrollbar = tk.Scrollbar(
+            self.canvas_frame, orient=tk.VERTICAL)
+        self.canvas_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.canvas_scrollbar.config(command=self.canvas.yview)
+
+        self.canvas.pack(expand=True, fill=tk.BOTH)
+
+        self.bottom_frame = tk.Frame(self.root, width=800, height=200)
+
+        self.label = tk.Label(self.bottom_frame, text=self.username)
         self.label.pack()
 
-        self.entry = tk.Entry(self.root)
+        self.entry = tk.Entry(self.bottom_frame, width=50)
         self.entry.pack()
 
-        self.button = tk.Button(self.root, text='Send',
-                                command=self.handle_click)
-        self.button.pack()
+        self.send_btn = tk.Button(
+            self.bottom_frame, text='Send', command=self.handle_click
+        )
+        self.send_btn.pack()
 
-        self.new_instance = tk.Button(
-            self.root, text='New window', command=self.create_new_instance)
-        self.new_instance.pack()
+        self.new_instance_btn = tk.Button(
+            self.bottom_frame, text='New window', command=lambda: singlethread(ChatRoom))
+        self.new_instance_btn.pack()
+
+        self.bottom_frame.pack()
 
         self.root.mainloop()
-
-        # self.loadall()
-        # while True:
-        #     messages = file_handler.read()
-        #     for line in messages:
-        #         self.canvas.create_text(0,0, text=line)
-
-        # while True:
-        #     self.listen()
-        #     time.sleep(2)
 
     def handle_click(self, event=None):
         msg = self.entry.get()
         self.entry.delete(0, tk.END)
         self.client.send_thread(msg)
 
-    def create_new_instance(self):
-        singlethread(ChatRoom)
+    def listen(self):
+        while True:
+            msg = file_handler.read()[-1]
+            if msg != self.prev_msg:
+                self.canvas.create_text(self.x, self.y, text=msg.split())
+                self.y += 30
+                self.prev_msg = msg
 
-    def listen2(self):
+    def _listen2(self):
+        """Less efficient, loads all messages before instance creation"""
         while True:
             messages = file_handler.read()
             for line in messages:
                 self.canvas.create_text(self.x, self.y, text=line)
                 self.y += 30
 
-    def loadall(self):
+    def _loadall(self):
+        """Loads text file into chatroom"""
         print(file_handler.read())
         for line in file_handler.read():
             self.canvas.create_text(text=line)
-
-    def listen(self):
-        while True:
-            msg = file_handler.read()[-1]
-            if msg != self.prev_msg:
-                self.canvas.create_text(self.x, self.y, text=msg)
-                self.y += 30
-                self.prev_msg = msg
--- modules/prompt_window.py
+++ modules/prompt_window.py
@@ -1,33 +1,41 @@
 import tkinter as tk
+import tkinter.messagebox as messagebox
 
 """Collects Client Name"""
 
 
 class PromptWindow:
     def __init__(self, parent, message, title='Prompt Window'):
-        self.message = message
-        self.title = title
-        self.parent = parent
-
+        self.message, self.title, self.parent = message, title, parent
         self.build()
 
     def build(self):
-
         self.root = tk.Tk()
         self.root.title(self.title)
+
         self.label = tk.Label(self.root, text=self.message)
         self.label.grid(row=0, column=0)
+
         self.entry = tk.Entry(self.root)
         self.entry.grid(row=1, column=0, padx=0, pady=5)
-        self.entry.bind('<Return>', lambda: self.on_button_press())
-        self.button = tk.Button(self.root, text='Confirm',
-                                command=self.on_button_press)
+        self.entry.bind('<Return>', lambda e: self.on_button_press())
+
+        self.button = tk.Button(
+            self.root, text='Confirm', command=self.on_button_press
+        )
         self.button.grid(row=2, column=0, pady=10)
+
         self.root.mainloop()
 
     def on_button_press(self):
-        self.parent.username = self.entry.get()
-        self.root.destroy()
+        user_input = self.entry.get()
+        if user_input != "":
+            self.parent.username = user_input
+            self.root.destroy()
+        else:
+            messagebox.showerror(
+                title="Input Error", message="Username cannot be null"
+            )
 
 
 if __name__ == "__main__":
